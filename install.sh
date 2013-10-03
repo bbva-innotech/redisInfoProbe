563331e9c2acfc44fdb7d3847c4c4f616e8aca5f
innotech <innotech@cfgmongodb02.(none)>
2013-10-03 10:39:07 +0200

    Install dependencies

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+apt-get -y install python-pip python-dev
+pip install psutil redis
 cp redisInfoProbeBase.cfg redisInfoProbe.cfg
 echo "tags = liberty,probe,`hostname`" >> redisInfoProbe.cfg
 cp redisInfoProbe_init.d /etc/init.d/redisInfoProbe
