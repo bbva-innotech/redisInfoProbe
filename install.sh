6bbdc7634d5dcda8cb79401cf25407311501cce4
innotech <innotech@cfgmongodb01.(none)>
2013-10-03 10:21:58 +0200

    Auto hostname in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+cp redisInfoProbeBase.cfg redisInfoProbe.cfg
+echo "tags = liberty,probe,`hostname`" >> redisInfoProbe.cfg
 cp redisInfoProbe_init.d /etc/init.d/redisInfoProbe
 chmod a+x /etc/init.d/redisInfoProbe
 update-rc.d redisInfoProbe defaults
-/etc/init.d/redisInfoProbe start
+/etc/init.d/redisInfoProbe restart
